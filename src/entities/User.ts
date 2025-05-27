8fa25c5e10e2c59a9f3ad482b04caeea8c4749b9
Cuong Tran <125862540+tom474@users.noreply.github.com>
2025-05-27 15:51:36 +0700

    Implement User and Account entities with relationships; add Vehicle entity with additional properties (#3)

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,16 +1,32 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { Entity, Column, PrimaryColumn, OneToOne } from "typeorm";
+import { Account } from "./Account";
 
 @Entity()
-export class Photo {
+export class User {
     @PrimaryColumn()
     id: number;
 
-    @Column()
+    @Column({ type: "varchar", length: 255 })
     name: string;
 
-    @Column()
+    @Column({ type: "varchar", length: 255, unique: true })
     email: string;
 
-    @Column()
+    @Column({ type: "varchar", length: 255 })
     password: string;
+
+    @Column({ type: "varchar", length: 255 })
+    profileImageUrl: string;
+
+    @Column({ type: "varchar", length: 15, unique: true })
+    phoneNumber: string;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    updatedAt: Date;
+
+    @OneToOne(() => Account, (account) => account.user)
+    account: Account;
 }
